6199dd607bc0cdc0f776d8ca1a94f5d33657dd1d
bit801 <house.nations@gmail.com>
2017-01-03 19:50:50 -0700

    goValues main created

---
+++ goValues.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//stings in goLang can be added together with +
+	fmt.Println("go" + "lang!!")
+
+	//integers and floats in goLang
+	fmt.Println("1=1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	//booleans with boolean operators
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
