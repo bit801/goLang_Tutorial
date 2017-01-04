d77179b2c48214645e6538b5a7fe268681c9066c
bit801 <house.nations@gmail.com>
2017-01-04 15:48:27 -0700

    stuff removed
    stuff removed

--- test/goConstants.go
+++
@@ -1,9 +0,0 @@
-//	Go supports constants of character, string, boolean, and numeric values
-
-package main
-
-import "fmt"
-import "math"
-
-//const declares a constant value
-const s string = "constant"
