bf8f769eedf5fd9ae6a5d204dbba6b9f3e1c0f69
bit801 <house.nations@gmail.com>
2017-01-05 16:05:55 -0700

    Edit goFor
    Added a classic initial/condition/after for loop and a for loop without
    a condition.

--- goFor/goFor.go
+++ goFor/goFor.go
@@ -4,10 +4,27 @@ import "fmt"
 
 func main() {
 
-	//The most basic type of for loop with a single condition
+	//the most basic type of for loop with a single condition
+	fmt.Println() //print blank line to screen
 	i := 1
 	for i <= 3 {
 		fmt.Println(i)
 		i = i + i
 	}
+	fmt.Println() //print blank line to screen
+
+	//a classic initial/condition/after for loop
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+	fmt.Println() //print blank line to screen
+
+	//for loop without a conditon will loop repeatedly until you break out of the loop
+	//or return from the enclosed function
+	for {
+		fmt.Println("loop without condition.")
+		break
+	}
+	fmt.Println() //print blank line to screen
+
 }
