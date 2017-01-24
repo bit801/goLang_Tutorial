0a5c607ab13f8e4319241643320e76950a66e152
bit801 <house.nations@gmail.com>
2017-01-23 20:48:09 -0700

    Edit for switch
    Added switch and three cases:
    “one”, “two”, “three”.

--- goSwitch/goSwitch.go
+++ goSwitch/goSwitch.go
@@ -6,4 +6,14 @@ import "fmt"
 
 func main() {
 
+	1 := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
 }
