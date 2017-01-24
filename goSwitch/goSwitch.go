1cbc2e22e943f4649d7704cf98c65fdad8358857
bit801 <house.nations@gmail.com>
2017-01-24 08:47:18 -0700

    Multiple expressions in case statement
    Added a switch with multiple expressions in the case statement. These
    are separated by commas.

--- goSwitch/goSwitch.go
+++ goSwitch/goSwitch.go
@@ -3,22 +3,35 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 
 	//This is a basic switch
+	//the := sytax is shorthand for declaring and initializing a variable,
+	//e.g. for var i int = 1 in this case
 	i := 1
 	fmt.Print("Write ", i, " as ")
-	switch i {
-	case 1:
+	switch i { //switch, if i is equal to
+	case 1: //must have colon after case: 1 go to case 1
 		fmt.Println("one")
-	case 2:
+	case 2: //2 go to case 2
 		fmt.Println("two")
-	case 3:
+	case 3: //3 go to case 3
 		fmt.Println("three")
 	}
-}
 
-//You can use commas to separate multiple expressions in
-//the same case statement. We use the optional 'default'
-//case in this example as well
+	//You can use commas to separate multiple expressions in
+	//the same case statement. We use the optional 'default'
+	//case in this example as well
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday: //comma used to separate expressions
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	//Switch without an expression is an alternate way to express if/else logic.
+	//Here we also show how the case expressions can be non-constants
+
+}
