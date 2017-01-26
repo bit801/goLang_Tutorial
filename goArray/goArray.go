0cf8ef9047c738cb6f76f689cf8130157e3eb967
bit801 <house.nations@gmail.com>
2017-01-26 16:20:09 -0700

    Finished
    Finished goSwitch by adding a ‘type switch.

---
+++ goArray/goArray.go
@@ -0,0 +1,11 @@
+//In Go, an array is a numbered sequence of elements of a specific length
+
+package main
+
+import "fmt"
+
+func main() {
+	//Here we create an array that will hold exactly 5 ints. The type of elements and length are both part of
+	//the array's type. By default an array is zero-valued, which for ints means
+
+}
--- goSwitch/goSwitch.go
+++ goSwitch/goSwitch.go
@@ -13,7 +13,7 @@ func main() {
 	i := 1
 	fmt.Print("Write ", i, " as ")
 	switch i { //switch, if i is equal to
-	case 1: //must have colon after case: 1 go to case 1
+	case 1: //must have colon after case: 1 go to case 1. Don't forget the colon after the case
 		fmt.Println("one")
 	case 2: //2 go to case 2
 		fmt.Println("two")
@@ -33,5 +33,29 @@ func main() {
 
 	//Switch without an expression is an alternate way to express if/else logic.
 	//Here we also show how the case expressions can be non-constants
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	//A "type switch" compares types instead of values. You can use this to discover the type
+	//of an interface value. In this example, the variable 't' will have the type corresponding
+	//to its clause.
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Println("Don't know the type %Tn", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 
 }
